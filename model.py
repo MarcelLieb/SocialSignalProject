c20c66a8b86e4af6ced91f145eb040497575156c
Marcel Lieb <marclie1@gmx.de>
2024-10-24 15:49:00 +0200

    features rename

--- model.py
+++ model.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 class GRUClassifier(nn.Module):
     def __init__(self, input_dim, gru_dim=32, num_gru_layers=2, hidden_size=16, bidirectional=True):
         super(GRUClassifier, self).__init__()
-        self.gru = nn.GRU(input_size=input_dim, hidden_size=gru_dim, num_layers=num_gru_layers,
-                          batch_first=True, bidirectional=bidirectional)
+        self.features = nn.GRU(input_size=input_dim, hidden_size=gru_dim, num_layers=num_gru_layers,
+                               batch_first=True, bidirectional=bidirectional)
         self.classification_head = nn.Sequential(
             nn.Dropout(0.5),
             nn.Linear(gru_dim, hidden_size),
@@ -14,7 +14,7 @@ class GRUClassifier(nn.Module):
         )
 
     def forward(self, X, lengths=None):
-        _, h_n = self.gru(X)  # NL, h_out
+        _, h_n = self.features(X)  # NL, h_out
         h_n = h_n[-1, :]
         logits = self.classification_head(h_n)
         return logits.squeeze(-1)

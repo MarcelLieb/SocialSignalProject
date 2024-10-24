9038fb47415dfcd998401d169a511cd9885cf16b
Marcel Lieb <marclie1@gmx.de>
2024-10-24 09:40:41 +0200

    initial dataset and model

---
+++ dataset.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+import numpy as np
+
+from functools import lru_cache
+
+
+FEATURES_DIR = "../data/features"
+
+
+# speed up loading time drastically
+@lru_cache(maxsize=16)
+def load_csv(file):
+    return pd.read_csv(file)
+
+
+def load_unimodal_data(label_df, features, undersample_negative=None):
+    feature_dir = f'{FEATURES_DIR}/{features}'
+    X = []
+    y = []
+    ids = []
+    current_segment = None
+    feature_df = None
+    if not (undersample_negative is None):
+        rng = np.random.default_rng(42)
+    for _, row in tqdm(label_df.iterrows()):
+        if not (undersample_negative is None) and row.humor == 0:
+            if rng.random() <= (1 - undersample_negative):
+                continue
+        if current_segment != row.segment:
+            current_segment = row.segment
+            feature_df = load_csv(f'{feature_dir}/{row.coach}/{row.segment}.csv')
+        seg_feature_df = feature_df[(feature_df.timestamp >= row.start) & (feature_df.timestamp < row.end)]
+        seg_features = seg_feature_df.iloc[:, 2:].values
+        seg_features = np.pad(seg_features, ((0, 4 - seg_features.shape[0]), (0, 0)))
+        seg_features = np.expand_dims(seg_features, 0)
+        X.append(seg_features)
+        y.append(row.humor)
+        ids.append(row.ID)
+    X = np.vstack(X)
+    y = np.array(y)
+    return X, y, ids
+
+
+class CustomDS(Dataset):
+
+    def __init__(self, X, y, ids, device: str = 'cpu'):
+        super(CustomDS, self).__init__()
+        self.device = device
+        self.X = torch.from_numpy(X.astype(np.float32)).to(self.device)
+        self.y = torch.from_numpy(y.astype(np.float32)).to(self.device)
+        self.ids = ids
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, item):
+        return self.X[item], self.y[item], self.ids[item]
---
+++ model.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+
+class GRUClassifier(nn.Module):
+    def __init__(self, input_dim, gru_dim=32, num_gru_layers=2, hidden_size=16, bidirectional=True):
+        super(GRUClassifier, self).__init__()
+        self.gru = nn.GRU(input_size=input_dim, hidden_size=gru_dim, num_layers=num_gru_layers,
+                          batch_first=True)
+        self.classification_head = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(gru_dim, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1)
+        )
+
+    def forward(self, X, lengths=None):
+        _, h_n = self.gru(X)  # NL, h_out
+        h_n = h_n[-1, :]
+        logits = self.classification_head(h_n)
+        return logits.squeeze(-1)
